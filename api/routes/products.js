9399a367814578735922ab6b31848e314c3cee08
Ismat-Samadov <ismetsemedov@gmail.com>
2024-01-21 14:13:33 +0400

    Adding More Routes to the API | Creating a REST API with Node.js

---
+++ api/routes/products.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling get requests to /products'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling post requests to /products'
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') { // Corrected typo from 'spcial' to 'special'
+        res.status(200).json({
+            message: 'you discovered special products',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'you passed ordinary products id'
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'product updated',
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'product deleted',
+    });
+});
+
+module.exports = router;
