dcaf9ae650c1c601d7c3294713fe125194ddb09e
Ismat-Samadov <ismetsemedov@gmail.com>
2024-01-24 00:20:39 +0400

    updated with done

--- api/models/product.js
+++ api/models/product.js
@@ -1,16 +1,10 @@
-// models/product.js
-
-// Importing the mongoose library
 const mongoose = require('mongoose');
 
-// Defining a Mongoose schema for the product
-const productSchema = new mongoose.Schema({
-    // Defining the 'name' field as a required String
+const productSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true },
-
-    // Defining the 'price' field as a required Number
-    price: { type: Number, required: true }
+    price: { type: Number, required: true },
+    productImage: { type: String, required: true }
 });
 
-// Creating a Mongoose model named 'Product' based on the productSchema
 module.exports = mongoose.model('Product', productSchema);
